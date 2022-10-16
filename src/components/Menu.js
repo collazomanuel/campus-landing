c50cf2b882f8a2878d4b245df7a85e01fbcaa0ba
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-10-16 17:12:50 -0300

    eighth commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,11 +6,21 @@ import MenuButton from './MenuButton'
 const Menu = () => {
 
   return (
-    <div className='middle-part button-container'>
-      <MenuButton type='Book' text='Grado' />
-      <MenuButton type='Hat' text='Posgrado' />
-      <MenuButton type='Teacher' text='Formación docente' />
-      <MenuButton type='Cetec' text='Cetec' />
+    <div className='middle-part'>
+      <div className='adverts-container'>
+        <div className='adverts-title'>
+          ANUNCIOS
+        </div>
+        <div className='adverts-content'>
+          Conocé los cursos de posgrado en Estabilidad en <a href='/'>https://www.fi.uba.ar/noticias/cursos-de-posgrado-del-depto-de-estabilidad-2</a>
+        </div>
+      </div>
+      <div className='buttons-container'>
+        <MenuButton type='Book' text='Grado' />
+        <MenuButton type='Hat' text='Posgrado' />
+        <MenuButton type='Teacher' text='Formación docente' />
+        <MenuButton type='Cetec' text='Cetec' />
+      </div>
     </div>
   )
 }
--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -24,4 +24,3 @@ const MenuButton = (props) => {
 }
 
 export default MenuButton
-
