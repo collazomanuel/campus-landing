4e94ea178002a1c463497b893e3209bace388002
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-12-06 08:56:45 -0300

    added correct links for buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,6 +9,12 @@ import Marquee from 'react-easy-marquee'
 
 const Menu = () => {
 
+
+  const linkGrado = 'https://campusgrado.fi.uba.ar/course/index.php?categoryid=2'
+  const linkPosgrado = 'https://campus.fi.uba.ar/course/index.php?categoryid=16'
+  const linkFormacionDocente = 'https://campusgrado.fi.uba.ar/course/index.php?categoryid=680'
+  const linkCetec = 'https://campusgrado.fi.uba.ar/course/index.php?categoryid=679'
+
   const announcement1 = 'Conocé los cursos de posgrado en Estabilidad en https://www.fi.uba.ar/noticias/cursos-de-posgrado-del-depto-de-estabilidad-2'
   const announcement2 = 'La FIUBA, presente en el XV COINI (más información en https://coiniarg.com)'
   const announcement3 = 'Nanomateriales y nanotecnología: La FIUBA invita a la disertación que ofrecerá el Dr. Galo Soler Illia sobre el tema'
@@ -28,10 +34,10 @@ const Menu = () => {
         </div>
       </div>
       <div className='buttons-container'>
-        <MenuButton type='Book' text='Grado' />
-        <MenuButton type='Hat' text='Posgrado' />
-        <MenuButton type='Teacher' text='Formación docente' />
-        <MenuButton type='Cetec' text='Cetec' />
+        <MenuButton type='Book' text='Grado' link={linkGrado} />
+        <MenuButton type='Hat' text='Posgrado' link={linkPosgrado} />
+        <MenuButton type='Teacher' text='Formación docente' link={linkFormacionDocente} />
+        <MenuButton type='Cetec' text='Cetec' link={linkCetec} />
       </div>
     </div>
   )
--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -7,17 +7,18 @@ const MenuButton = (props) => {
 
   const text = props.text
   const type = props.type
+  const link = props.link
 
   if (type === 'Cetec') {
     return (
-      <a className={'menu-button' + ' ' + type.toLowerCase() + '-button'} href='/'>
+      <a className={'menu-button' + ' ' + type.toLowerCase() + '-button'} href={link}>
         <Icon type={type} />
       </a>
     )
   }
 
   return (
-    <a className={'menu-button' + ' ' + type.toLowerCase() + '-button'} href='/'>
+    <a className={'menu-button' + ' ' + type.toLowerCase() + '-button'} href={link}>
       <Icon type={type} /> <div className='button-text'>{text}</div>
     </a>
   )
